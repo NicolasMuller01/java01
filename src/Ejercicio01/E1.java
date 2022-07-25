e3cf300cf69d725d88e76a27f89303dfa1d509c7
Nicolas Müller <nicolasmatiasmuller19@gmail.com>
2022-07-25 15:32:55 -0300

    first commit

---
+++ src/Ejercicio01/E1.java
@@ -0,0 +1,22 @@
+package Ejercicio01;
+
+public class E1 {
+    public static void main(String[] args) {
+        //1.1 enteros
+        byte v1 = 5;
+        short v2 = 19;
+        int v3 = 0;
+        long v4 = 0;
+
+        //1.2 decimales
+        float v5 = 1.0f;
+        double v6 = 1.0;
+
+        //2 bolean
+        boolean v7 = false;
+
+        //texto
+        char v8 = 'a';
+        String v9 = "a";
+        }
+}
