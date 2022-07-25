a97415d816e087883e3005ec4ad04dfb1055a1af
Nicolas Müller <nicolasmatiasmuller19@gmail.com>
2022-07-25 16:24:02 -0300

    0.1

---
+++ src/Ejercicio2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int price = entrada.nextInt();
+        iva(price);
+    }
+    static void iva(int price) {
+        System.out.println("Price: " + (price+(price*0.27)));
+    }
+}
