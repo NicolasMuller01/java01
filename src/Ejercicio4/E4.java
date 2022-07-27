0539585feb7c51ee96dc63ba929f409b3f53e5b2
Nicolas Müller <nicolasmatiasmuller19@gmail.com>
2022-07-27 15:06:36 -0300

    0.4

--- src/Ejercicio4/E4.java
+++ src/Ejercicio4/E4.java
@@ -1,7 +1,16 @@
 package Ejercicio4;
 
+import java.util.Scanner;
+
 public class E4 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        if(name.equals("nicolas")){
+            System.out.println("Nicolas");
+        }
+
         SmartDevice device = new SmartDevice("alexa","audio","11","2022");
         SmartWatch watch = new SmartWatch("SWa22","android","a22","2021",11);
         SmartPhone phone = new SmartPhone("iphone11","iphone","11","2019","ios");
---
+++ src/Ejercicio5/Main.java
@@ -0,0 +1,10 @@
+package Ejercicio5;
+
+public class Main {
+    public static void main(String[] args) {
+        CocheCRUD cocheCrud = new CocheCRUDImpl();
+        cocheCrud.save();
+        cocheCrud.delete();
+        cocheCrud.findAll();
+    }
+}
